5f651529e13699985be423528fa3fbc0ff128a7c
dingo sky <paul@dingosky.com>
2022-09-12 15:29:17 -0600

    Initial Commit

---
+++ Sharktopoda/SharktopodaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharktopodaApp.swift
+//  Sharktopoda
+//
+//  Created by Paul Rogers on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SharktopodaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

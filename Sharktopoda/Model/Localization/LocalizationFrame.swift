58bd3d49a270116c0494ccbb2d62f9a7ba8cbcb6
dingo sky <paul@dingosky.com>
2022-10-17 16:45:18 -0600

    Remove ordered in favor of frames

--- Sharktopoda/Model/Localization/LocalizationFrame.swift
+++ Sharktopoda/Model/Localization/LocalizationFrame.swift
@@ -10,39 +10,27 @@ import Foundation
 struct LocalizationFrame: Comparable {
   let frameNumber: Int
   
-  var id: String? = nil
-  var ids: [String]? = nil
+  private(set) var ids: [String]
   
-  init(using localization: Localization, frameDuration: Int) {
-    id = localization.id
-    frameNumber = LocalizationSet.frameNumber(elapsedTime: localization.elapsedTime,
-                                              frameDuration: frameDuration)
+  init(_ localization: Localization, _ frameNumber: Int) {
+    self.frameNumber = frameNumber
+    ids = [String]()
+    ids.append(localization.id)
   }
   
   mutating func add(_ localization: Localization) {
-    if let id = id {
-      ids = [id, localization.id]
-      self.id = nil
-    } else {
-      ids!.append(localization.id)
-    }
+    ids.append(localization.id)
   }
   
   mutating func remove(_ localization: Localization) {
-    if let _ = id {
-      self.id = nil
-    } else {
-      guard let index = ids!.firstIndex(of: localization.id) else { return }
-      ids!.remove(at: index)
-      if ids?.count == 1 {
-        self.id = ids![0]
-        ids = nil
-      }
-    }
+    guard let index = ids.firstIndex(of: localization.id) else { return }
+    ids.remove(at: index)
   }
-  
+    
   // Comparable
   static func < (lhs: LocalizationFrame, rhs: LocalizationFrame) -> Bool {
     lhs.frameNumber < rhs.frameNumber
   }
+  
+  
 }
--- Sharktopoda/Model/Localization/LocalizationSet.swift
+++ Sharktopoda/Model/Localization/LocalizationSet.swift
@@ -9,10 +9,8 @@ import Foundation
 
 struct LocalizationSet {
   private var storage = [String : Localization]()
-  private var ordered = [OrderedLocalization]()
-  private var selected = Set<String>()
-
   private var frames = [LocalizationFrame]()
+  private var selected = Set<String>()
   
   let frameDuration: Int
   
@@ -32,17 +30,28 @@ struct LocalizationSet {
 
 // Retrieval
 extension LocalizationSet {
+  func localizationFrames(at elapsedTime: Int,
+                          for duration: Int,
+                          stepping direction: Step) -> [LocalizationFrame] {
+    let startIndex = frameIndex(elapsedTime: elapsedTime)
+    let endTime = elapsedTime + (direction == .right ? 1 : -1) * duration
+    let endIndex = frameIndex(elapsedTime: endTime)
+    
+    let range = direction == .right ? startIndex..<endIndex : endIndex..<startIndex
+
+    return Array(frames[range])
+  }
+  
   func localizations(at elapsedTime: Int,
                      for duration: Int,
                      stepping direction: Step) -> [Localization] {
-    let clusterIndex = insertionIndex(for: elapsedTime)
-    let startIndex = slide(clusterIndex, direction)
-    let endIndex = slide(clusterIndex, direction.opposite(), until: elapsedTime + duration)
+    let localizationFrames = localizationFrames(at: elapsedTime, for: duration, stepping: direction)
+
+    let localizations = localizationFrames.reduce(into: [Localization]()) { acc, frame in
+      acc.append(contentsOf: frame.ids.map { storage[$0]! })
+    }
     
-    let range = startIndex < endIndex ? startIndex..<endIndex : endIndex..<startIndex
-    let localizations = localizations(in: range)
-    
-    return startIndex < endIndex ? localizations : localizations.reversed()
+    return direction == .right ? localizations : localizations.reversed()
   }
 }
 
@@ -52,8 +61,6 @@ extension LocalizationSet {
     guard !exists(localization) else { return false }
     
     storage[localization.id] = localization
-    addOrdered(localization)
-    
     framesInsert(localization)
     
     return true
@@ -61,7 +68,7 @@ extension LocalizationSet {
   
   mutating func clear() {
     storage.removeAll()
-    ordered.removeAll()
+    frames.removeAll()
     selected.removeAll()
   }
   
@@ -69,21 +76,19 @@ extension LocalizationSet {
     guard let localization = storage[id] else { return false }
     
     storage[id] = nil
-    removeOrdered(localization)
+    frameRemove(localization)
     selected.remove(localization.id)
 
     return true
   }
     
   mutating func update(_ localization: Localization) -> Bool {
-    let existing = storage[localization.id]
-    
-    storage[localization.id] = localization
-    
-    if (localization.elapsedTime != existing?.elapsedTime) {
-      removeOrdered(localization)
-      addOrdered(localization)
+    if let existing = storage[localization.id],
+       localization.elapsedTime != existing.elapsedTime {
+      frameRemove(existing)
+      framesInsert(localization)
     }
+    storage[localization.id] = localization
     
     return true
   }
@@ -91,22 +96,22 @@ extension LocalizationSet {
 
 /// Localization frames
 extension LocalizationSet {
-  static func frameNumber(elapsedTime: Int, frameDuration: Int) -> Int {
-    (elapsedTime - 1) / frameDuration + 1
-  }
-  
   func frameNumber(for localization: Localization) -> Int {
-    (localization.elapsedTime - 1) / frameDuration + 1
+    frameNumber(elapsedTime: localization.elapsedTime)
+  }
+
+  func frameNumber(elapsedTime: Int) -> Int {
+    (elapsedTime - 1) / frameDuration + 1
   }
   
   private mutating func framesInsert(_ localization: Localization) {
     if frames.isEmpty {
-      frames.insert(LocalizationFrame(using: localization, frameDuration: frameDuration),
+      frames.insert(LocalizationFrame(localization, frameDuration),
                     at: 0)
     } else {
       let index = frameIndex(for: localization)
       if index == frames.count {
-        frames.insert(LocalizationFrame(using: localization, frameDuration: frameDuration),
+        frames.insert(LocalizationFrame(localization, frameDuration),
                       at: index)
       } else {
         var localizationFrame = frames[index]
@@ -115,7 +120,7 @@ extension LocalizationSet {
           localizationFrame.add(localization)
           frames[index] = localizationFrame
         } else {
-          frames.insert(LocalizationFrame(using: localization, frameDuration: frameDuration),
+          frames.insert(LocalizationFrame(localization, frameDuration),
                         at: index)
         }
       }
@@ -131,14 +136,14 @@ extension LocalizationSet {
     }
   }
   
-  private func frameIndex(for localization: Localization) -> Int {
+  private func frameIndex(elapsedTime: Int) -> Int {
     var left = 0
     var right = frames.count - 1
     
     var index = 0
     var found = 0
     
-    let frameNumber = frameNumber(for: localization)
+    let frameNumber = frameNumber(elapsedTime: elapsedTime)
     
     while left <= right {
       index = (left + right) / 2
@@ -155,49 +160,10 @@ extension LocalizationSet {
     
     return found < frameNumber ? left : right + 1
   }
-}
 
-/// Order
-extension LocalizationSet {
-  private mutating func addOrdered(_ localization: Localization) {
-    let orderedLocalization = OrderedLocalization(for: localization)
-    
-    if ordered.isEmpty {
-      ordered.append(orderedLocalization)
-    } else if ordered.count == 1 {
-      if orderedLocalization < ordered[0] {
-        ordered.insert(orderedLocalization, at: 0)
-      } else {
-        ordered.append(orderedLocalization)
-      }
-    } else if ordered.last! < orderedLocalization {
-      // Since localizations will often be added at the end, this optimization seems sensible
-      ordered.append(orderedLocalization)
-    } else {
-      let index = insertionIndex(for: orderedLocalization)
-      ordered.insert(orderedLocalization, at: index)
-    }
-  }
   
-  private mutating func removeOrdered(_ localization: Localization) {
-    guard !ordered.isEmpty else { return }
-
-    let orderedLocalization = OrderedLocalization(for: localization)
-    
-    if ordered.count == 1 {
-      guard orderedLocalization == ordered[0] else { return }
-      ordered.remove(at: 0)
-    } else {
-      let index = insertionIndex(for: orderedLocalization)
-      // Since there can be multiple Localizations at a specified time and the insertion index
-      // any one of the OrderedLocalizations at that time, we need to find the specfic
-      // OrderedLocalization we really want.
-      if let localizationIndex = findIndex(matching: orderedLocalization, clusteredAt: index) {
-        ordered.remove(at: localizationIndex)
-      } else {
-        return
-      }
-    }
+  private func frameIndex(for localization: Localization) -> Int {
+    frameIndex(elapsedTime: localization.elapsedTime)
   }
 }
 
@@ -226,83 +192,5 @@ extension LocalizationSet {
 extension LocalizationSet {
   func exists(_ localization: Localization) -> Bool {
     storage[localization.id] != nil
-  }
-  
-  private func localizations(in range: Range<Int>) -> [Localization] {
-    ordered[range]
-      .map { $0.id }
-      .map { storage[$0]! }
-  }
-}
-
-/// Localization index processing
-extension LocalizationSet {
-  
-  private func findIndex(matching localization: OrderedLocalization, clusteredAt index: Int) -> Int? {
-    if let localizationIndex = findIndex(matching: localization,
-                                         startingAt: index,
-                                         direction: .right) {
-      return localizationIndex
-    } else
-    if let localizationIndex = findIndex(matching: localization,
-                                         startingAt: index - 1,
-                                         direction: .left) {
-      return localizationIndex
-    }
-    return nil
-  }
-  
-  private func findIndex(matching localization: OrderedLocalization,
-                         startingAt index: Int,
-                         direction: Step) -> Int? {
-    var foundLocalization = ordered[index]
-    
-    var foundIndex = index
-    while foundLocalization.elapsedTime == localization.elapsedTime {
-      if foundLocalization.id == localization.id {
-        return foundIndex
-      }
-      foundIndex += direction.rawValue
-      foundLocalization = ordered[foundIndex]
-    }
-    return nil
-  }
-  
-  private func insertionIndex(for localization: OrderedLocalization) -> Int {
-    insertionIndex(for: localization.elapsedTime)
-  }
-  
-  private func insertionIndex(for elapsedTime: Int) -> Int {
-    var left = 0
-    var right = ordered.count - 1
-
-    var index = 0
-    var value: Int = .min
-    
-    while left < right {
-      index = (left + right) / 2
-      value = ordered[index].elapsedTime
-      
-      if value == elapsedTime {
-        return index
-      } else
-      if value < elapsedTime {
-        left = slide(index, .right) + 1
-      } else {
-        right = slide(index, .left) - 1
-      }
-    }
-    
-    return value < left ? left : right + 1
-  }
-  
-  private func slide(_ index: Int, _ direction: Step, until time: Int? = nil) -> Int {
-    let elapsedTime = time ?? ordered[index].elapsedTime
-    
-    var slideIndex = index
-    while ordered[slideIndex + direction.rawValue].elapsedTime == elapsedTime {
-      slideIndex += direction.rawValue
-    }
-    return slideIndex
   }
 }

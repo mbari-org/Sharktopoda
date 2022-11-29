22557749b06f188bdd635e7920b57f17f8514a83
dingo sky <paul@dingosky.com>
2022-11-29 13:37:01 -0700

    Factor out PlayerView proxy methods

--- Sharktopoda/View/Video/VideoView/PlayerView/PlayerView.swift
+++ Sharktopoda/View/Video/VideoView/PlayerView/PlayerView.swift
@@ -5,7 +5,6 @@
 //  Apache License 2.0 — See project LICENSE file
 //
 
-import AVKit
 import SwiftUI
 
 struct PlayerView: NSViewRepresentable {
@@ -21,40 +20,3 @@ struct PlayerView: NSViewRepresentable {
   func updateNSView(_ nsView: NSViewType, context: Context) {}
 }
  
-extension PlayerView {
-  func clear() {
-    nsPlayerView.clear()
-  }
-
-  func clear(localizations: [Localization]) {
-    nsPlayerView.clear(localizations: localizations)
-  }
-
-  func clearConcept() {
-    nsPlayerView.conceptLayer?.removeFromSuperlayer()
-  }
-  
-  var currentTime: Int {
-    nsPlayerView.currentTime
-  }
-  
-  func display(localization: Localization) {
-    nsPlayerView.display(localization: localization)
-  }
-
-  func display(localizations: [Localization]) {
-    nsPlayerView.display(localizations: localizations)
-  }
-
-  var playerLayer: AVPlayerLayer {
-    nsPlayerView.playerLayer
-  }
-  
-  var showLocalizations: Bool {
-    nsPlayerView.showLocalizations
-  }
-  
-  var videoRect: CGRect {
-    nsPlayerView.videoRect
-  }
-}
---
+++ Sharktopoda/View/Video/VideoView/PlayerView/PlayerViewProxy.swift
@@ -0,0 +1,46 @@
+//
+//  PlayerViewProxy.swift
+//  Created for Sharktopoda on 11/29/22.
+//
+//  Apache License 2.0 — See project LICENSE file
+//
+
+import AVKit
+
+extension PlayerView {
+  func clear() {
+    nsPlayerView.clear()
+  }
+  
+  func clear(localizations: [Localization]) {
+    nsPlayerView.clear(localizations: localizations)
+  }
+  
+  func clearConcept() {
+    nsPlayerView.conceptLayer?.removeFromSuperlayer()
+  }
+  
+  var currentTime: Int {
+    nsPlayerView.currentTime
+  }
+  
+  func display(localization: Localization) {
+    nsPlayerView.display(localization: localization)
+  }
+  
+  func display(localizations: [Localization]) {
+    nsPlayerView.display(localizations: localizations)
+  }
+  
+  var playerLayer: AVPlayerLayer {
+    nsPlayerView.playerLayer
+  }
+  
+  var showLocalizations: Bool {
+    nsPlayerView.showLocalizations
+  }
+  
+  var videoRect: CGRect {
+    nsPlayerView.videoRect
+  }
+}

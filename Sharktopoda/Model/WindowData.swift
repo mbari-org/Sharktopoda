c81f1047d66ec252ccde9ce66a4627966ed49df6
dingo sky <paul@dingosky.com>
2022-12-28 09:43:50 -0700

    Pause on frame time

--- Sharktopoda/Model/WindowData.swift
+++ Sharktopoda/Model/WindowData.swift
@@ -82,12 +82,16 @@ extension WindowData {
   
   func pause(_ withDisplay: Bool = true) {
     guard !videoControl.paused else { return }
+
     play(rate: 0.0)
     playerView.clear()
     localizationData.clearSelected()
     
-    if withDisplay {
-      displayPaused()
+    let frameTime = currentFrameTime
+    videoControl.seek(elapsedTime: frameTime) { [weak self] done in
+      if withDisplay {
+        self?.displayPaused()
+      }
     }
   }
   
@@ -116,10 +120,8 @@ extension WindowData {
 
   func seek(elapsedTime: Int) {
     let frameTime = localizationData.frameTime(of: elapsedTime)
-
     pause(false)
-    playerView.clear()
-    localizationData.clearSelected()
+    
     videoControl.seek(elapsedTime: frameTime) { [weak self] done in
       self?.displayPaused()
     }
